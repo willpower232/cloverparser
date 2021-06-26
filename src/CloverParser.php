a38547a9d4512ad6462024b7798142452d30bf50
William Hall <github@willpoweredinc.co.uk>
2021-06-26 16:00:29 +0100

    🎉 everything

---
+++ src/CloverParser.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace WillPower232\CloverParser;
+
+use SimpleXMLElement;
+
+class CloverParser
+{
+    /** @var array<string> */
+    protected array $files = [];
+
+    /** @var array<SimpleXMLElement> */
+    protected array $xmlInstances = [];
+
+    /** @var array<string,int> */
+    protected array $totals = [];
+
+    /** @var array<string,int> */
+    protected array $coveredTotals = [];
+
+    private bool $hasCalculatedTotals = false;
+
+    public function addFile(string $pathToFile): self
+    {
+        if (!file_exists($pathToFile)) {
+            throw new \RuntimeException('Clover file is not present');
+        }
+
+        try {
+            $this->xmlInstances[] = new SimpleXMLElement($pathToFile, 0, true);
+        } catch (\Exception $e) {
+            throw new \RuntimeException('Unable to process Clover file', 0, $e);
+        }
+
+        $this->files[] = $pathToFile;
+
+        $this->hasCalculatedTotals = false;
+
+        return $this;
+    }
+
+    protected function reset(): void
+    {
+        $this->totals = [
+            'methods' => 0,
+            'conditionals' => 0,
+            'statements' => 0,
+            'elements' => 0,
+        ];
+
+        $this->coveredTotals = [
+            'coveredmethods' => 0,
+            'coveredconditionals' => 0,
+            'coveredstatements' => 0,
+            'coveredelements' => 0,
+        ];
+
+        $this->hasCalculatedTotals = false;
+    }
+
+    public function calculateTotals(): self
+    {
+        $this->reset();
+
+        foreach ($this->xmlInstances as $cloverXML) {
+            foreach ($cloverXML->xpath('//project') as $project) {
+                // no other way of seeing if the element exists?
+                if ($project->metrics->count() > 0) {
+                    $this->incrementTotalsWithMetrics($project->metrics);
+
+                    continue;
+                }
+
+                foreach ($project->xpath('//file') as $file) {
+                    // no other way of seeing if the element exists?
+                    if ($file->metrics->count() > 0) {
+                        $this->incrementTotalsWithMetrics($file->metrics);
+                    }
+                }
+            }
+        }
+
+        $this->hasCalculatedTotals = true;
+
+        return $this;
+    }
+
+    public function totalsCalculated(): bool
+    {
+        return $this->hasCalculatedTotals;
+    }
+
+    protected function incrementTotalsWithMetrics(SimpleXMLElement $metrics): void
+    {
+        $this->totals['methods'] += (int) $metrics['methods'];
+        $this->coveredTotals['coveredmethods'] += (int) $metrics['coveredmethods'];
+        $this->totals['conditionals'] += (int) $metrics['conditionals'];
+        $this->coveredTotals['coveredconditionals'] += (int) $metrics['coveredconditionals'];
+        $this->totals['statements'] += (int) $metrics['statements'];
+        $this->coveredTotals['coveredstatements'] += (int) $metrics['coveredstatements'];
+        $this->totals['elements'] += (int) $metrics['elements'];
+        $this->coveredTotals['coveredelements'] += (int) $metrics['coveredelements'];
+    }
+
+    public function getPercentage(): float
+    {
+        if ($this->totalsCalculated() === false) {
+            $this->calculateTotals();
+        }
+
+        $totals = array_sum($this->totals);
+
+        if ($totals === 0) {
+            return 0;
+        }
+
+        $coveredTotals = array_sum($this->coveredTotals);
+
+        return ($coveredTotals / $totals) * 100;
+    }
+}
---
+++ tests/CloverParserTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace WillPower232\CloverParser\Tests;
+
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use WillPower232\CloverParser\CloverParser;
+
+class CloverParserTest extends TestCase
+{
+    /**
+     * @return array<mixed>
+     */
+    private function makeTempFileSafely(): array
+    {
+        $file = tmpfile();
+
+        if ($file === false) {
+            throw new \Exception('Unable to create temporary file');
+        }
+
+        $path = stream_get_meta_data($file)['uri'];
+
+        return [
+            $file,
+            $path,
+        ];
+    }
+
+    public function testNoFile(): void
+    {
+        $parser = new CloverParser();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Clover file is not present');
+
+        $parser->addFile('/this/file/does/not/exist');
+    }
+
+    public function testNotXmlFile(): void
+    {
+        $parser = new CloverParser();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Unable to process Clover file');
+
+        [$file, $path] = $this->makeTempFileSafely();
+
+        $parser->addFile($path);
+    }
+
+    public function testTotalsHappenFromPercentage(): void
+    {
+        $parser = new CloverParser();
+
+        $this->assertFalse($parser->totalsCalculated());
+
+        $parser->getPercentage();
+
+        $this->assertTrue($parser->totalsCalculated());
+    }
+
+    public function testCalculationFromProject(): void
+    {
+        $parser = new CloverParser();
+
+        [$file, $path] = $this->makeTempFileSafely();
+
+        $clover = <<<ENDCLOVER
+        <?xml version="1.0" encoding="UTF-8"?>
+        <coverage generated="1618905787">
+            <project timestamp="1618905787">
+                <metrics files="29" loc="1846" ncloc="1233" classes="20" methods="58"
+                coveredmethods="50" conditionals="0" coveredconditionals="0" statements="414"
+                coveredstatements="316" elements="472" coveredelements="366"/>
+            </project>
+        </coverage>
+        ENDCLOVER;
+
+        fwrite($file, $clover);
+
+        $parser->addFile($path);
+
+        $this->assertSame(77.54237288135593, $parser->getPercentage());
+    }
+
+    public function testCalculationFromFiles(): void
+    {
+        $parser = new CloverParser();
+
+        [$file, $path] = $this->makeTempFileSafely();
+
+        $clover = <<<ENDCLOVER
+        <?xml version="1.0" encoding="UTF-8"?>
+        <coverage generated="1618905787">
+            <project timestamp="1618905787">
+                <package name="App\Providers">
+                    <file name="/var/www/app/Providers/AuthServiceProvider.php">
+                        <metrics loc="30" ncloc="18" classes="1" methods="1" coveredmethods="1"
+                        conditionals="1" coveredconditionals="0" statements="2" coveredstatements="1"
+                        elements="3" coveredelements="1"/>
+                    </file>
+                    <file name="/var/www/app/Providers/RouteServiceProvider.php">
+                        <metrics loc="56" ncloc="28" classes="1" methods="3" coveredmethods="1"
+                        conditionals="1" coveredconditionals="0" statements="8" coveredstatements="1"
+                        elements="11" coveredelements="1"/>
+                    </file>
+                </package>
+                <file name="/var/www/app/helpers.php">
+                    <metrics loc="54" ncloc="23" classes="0" methods="0" coveredmethods="0"
+                    conditionals="1" coveredconditionals="0" statements="4" coveredstatements="1"
+                    elements="4" coveredelements="1"/>
+                </file>
+            </project>
+        </coverage>
+        ENDCLOVER;
+
+        fwrite($file, $clover);
+
+        $parser->addFile($path);
+
+        $this->assertSame(20.51282051282051, $parser->getPercentage());
+    }
+
+    public function testCalculationAvoidsDivisionByZero(): void
+    {
+        $parser = new CloverParser();
+
+        [$file, $path] = $this->makeTempFileSafely();
+
+        $clover = <<<ENDCLOVER
+        <?xml version="1.0" encoding="UTF-8"?>
+        <coverage generated="1618905787">
+            <project timestamp="1618905787">
+                <metrics files="29" loc="1846" ncloc="1233" classes="20" methods="0"
+                coveredmethods="5" conditionals="0" coveredconditionals="0" statements="0"
+                coveredstatements="3" elements="0" coveredelements="6"/>
+            </project>
+        </coverage>
+        ENDCLOVER;
+
+        fwrite($file, $clover);
+
+        $parser->addFile($path);
+
+        $this->assertSame(0.0, $parser->getPercentage());
+    }
+}
